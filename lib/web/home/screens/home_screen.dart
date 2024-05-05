b6e997a1bce60fa540e19a36bcddef5928b0d20c
Mr-Talha110 <talhalatif636@gmail.com>
2024-05-05 18:43:44 +0500

    started creating homepage

--- lib/web/home/screens/home_screen.dart
+++ lib/web/home/screens/home_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/services/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: const Column(
+        children: [
+          SizedBox(height: 46),
+          Row(
+            children: [],
+          )
+        ],
+      ),
+    );
   }
 }
